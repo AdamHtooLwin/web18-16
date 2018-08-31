9cd125c58ee9db6f314eba5cf252d37960b67ae6
AdamHtooLwin <adamhtoolwin@gmail.com>
2018-08-31 19:16:24 +0700

    Added static index page and dividebyzero exception

--- PS1/app/controllers/application_controller.rb
+++ PS1/app/controllers/application_controller.rb
@@ -1,7 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  def divide_by_zero
-    logger.debug "IT'S A TRAP!"
-    a = 5/0
-  end
 end
--- PS1/config/routes.rb
+++ PS1/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "application#divide_by_zero"
+  # root "static#index"
+  # root application#divide_by_zero
+
+  get "/basics" => "static#index"
+
+  get "/basics/divide_by_zero" => "exception#divide_by_zero"
 end

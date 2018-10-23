6722e205b5d5ad6d0e1c3fc6c26cb0c48fab4050
AdamHtooLwin <adamhtoolwin@gmail.com>
2018-10-23 22:24:28 +0700

    Added basic registration feature

---
+++ basics/features/registration.feature
@@ -0,0 +1,13 @@
+Feature: User Registration
+
+  I, an unregistered user, wants to sign up for the website in order to be able to use it.
+
+  Scenario: Register a new user
+
+    An unregistered user should be able to register into the database.
+
+    Given I am an unregistered user
+    When I visit the registration page
+    Then I should see a form to register
+    When I submit the form
+    Then I should be redirected to the login page
---
+++ basics/features/step_definitions/registration_steps.rb
@@ -0,0 +1,20 @@
+Given("I am an unregistered user") do
+  pending # Write code here that turns the phrase above into concrete actions
+
+end
+
+When("I visit the registration page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see a form to register") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I submit the form") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be redirected to the login page") do
+  pending # Write code here that turns the phrase above into concrete actions
+end

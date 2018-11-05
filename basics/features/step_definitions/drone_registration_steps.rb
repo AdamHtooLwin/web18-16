fee2f2bb611a32704b3c932de7b10be61a0df5bf
AdamHtooLwin <adamhtoolwin@gmail.com>
2018-11-05 23:10:39 +0700

    Added class name to registration drone form

--- basics/features/step_definitions/drone_registration_steps.rb
+++ basics/features/step_definitions/drone_registration_steps.rb
@@ -1,10 +1,9 @@
 When("I visit the drone registration page") do
-
-  pending # Write code here that turns the phrase above into concrete actions
+  visit '/ps5/drone_registration'
 end
 
 Then("I should see a form to register the drone") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_selector('form#registration_form')
 end
 
 When("I submit the drone form") do
